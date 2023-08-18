3bbdf9da4206c4588bb1a7eb217c351cee94abba
LyokoJeremie <lucheng989898@gmail.com>
2023-08-18 08:20:50 +0800

    .

--- index.js
+++ index.js
@@ -4,16 +4,41 @@ const github = require('@actions/github');
 const copy = require('copy');
 
 try {
-    const sourceFile = core.getInput('source');
+    const sourceFile = core.getMultilineInput('source');
     const targetFile = core.getInput('target');
+    const cwd = core.getInput('cwd', {required: false}) || undefined;
+    const destBase = core.getInput('destBase', {required: false}) || undefined;
+    const modeOne = core.getBooleanInput('one', {required: false}) || false;
+    const modeMulti = core.getBooleanInput('multi', {required: false}) || false;
     console.log(`Copying "${sourceFile}" to "${targetFile}"`);
+    console.log(`modeOne:${modeOne} , modeMulti:${modeMulti}`);
+    console.log(`cwd:${cwd} , destBase:${destBase}`);
 
-    copy(sourceFile, targetFile, (err, files) => {
-        if (err) {
-            core.setFailed(err);
-        }
-        console.log(files);
-    });
+    if (modeOne) {
+        console.log(`run in modeOne`);
+        copy.one(sourceFile[0], targetFile, {cwd: cwd, destBase: destBase}, (err, files) => {
+            if (err) {
+                core.setFailed(err);
+            }
+            console.log(files);
+        });
+    } else if (modeMulti) {
+        console.log(`run in modeMulti`);
+        copy.each(sourceFile, targetFile, (err, files) => {
+            if (err) {
+                core.setFailed(err);
+            }
+            console.log(files);
+        });
+    } else {
+        console.log(`run in normal`);
+        copy(sourceFile[0], targetFile, (err, files) => {
+            if (err) {
+                core.setFailed(err);
+            }
+            console.log(files);
+        });
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
